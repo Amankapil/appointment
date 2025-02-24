fd82ee76eafbc7aa6e6bf81ca53515840ce55a77
Aman kapil <amankapil60@gmail.com>
2025-02-24 21:26:25 +0530

    fhalhj

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         salernomi: ["Salernomi J"],
+//         inter: ["Inter Display", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [require("@tailwindcss/forms")],
+// };
+
 /** @type {import('tailwindcss').Config} */
-export default {
+const tailwindConfig = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,3 +33,5 @@ export default {
   },
   plugins: [require("@tailwindcss/forms")],
 };
+
+export default tailwindConfig;
