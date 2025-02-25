fbd44dc3437801867fccbee2b70d95fc11b662cb
Aman kapil <amankapil60@gmail.com>
2025-02-25 16:53:17 +0530

    change in config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,29 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         salernomi: ["Salernomi J"],
+//         inter: ["Inter Display", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [require("@tailwindcss/forms")],
+// };
+
 /** @type {import('tailwindcss').Config} */
-export default {
+const tailwindConfig = {
   important: true,
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all files in the src directory
   ],
 
   theme: {
@@ -59,78 +78,10 @@ export default {
       },
     },
   },
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       salernomi: ["Salernomi J"],
-  //       inter: ["Inter Display", "sans-serif"],
-  //     },
-  //   },
-  // },
-  plugins: [require("@tailwindcss/forms")],
+  corePlugins: {
+    preflight: true,
+  },
+  safelist: [{ pattern: /.*/ }],
 };
 
-// /** @type {import('tailwindcss').Config} */
-// const tailwindConfig = {
-//   important: true,
-//   content: ["./src/components/**/*.{js,ts,jsx}", "./src/app/**/*.{js,ts,jsx}"],
-
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         salernomi: ["Salernomi J"],
-//         inter: ["Inter Display", "sans-serif"],
-//       },
-//       colors: {
-//         customBg: "var(--bg-custom)",
-//         customText: "var(--text-custom)",
-//         darkHover: "var(--text-dark-hover)",
-//       },
-//       fontSize: {
-//         custom64: "var(--text-64)",
-//         custom56: "var(--text-56)",
-//         custom20: "var(--text-20)",
-//       },
-//       width: {
-//         custom50: "var(--w-60)",
-//         custom75: "var(--w-200)",
-//         custom90: "var(--w-40)",
-//         w540: "540px",
-//         680: "680px",
-//         614: "614px",
-//         1200: "1200px",
-//         500: "500px",
-//         582: "582px",
-//         550: "550px",
-//       },
-//       height: {
-//         custom400: "var(--h-400)",
-//         custom600: "var(--h-600)",
-//         h540: "540px",
-//         680: "680px",
-//         1200: "1200px",
-//         500: "500px",
-//         276: "276px",
-//         308: "308px",
-//         340: "340px",
-//       },
-//       padding: {
-//         custom10: "var(--p-10)",
-//         custom20: "var(--p-20)",
-//       },
-//       margin: {
-//         custom15: "var(--m-15)",
-//         custom30: "var(--m-30)",
-//       },
-//       borderRadius: {
-//         customLg: "var(--rounded-lg)",
-//       },
-//     },
-//   },
-//   corePlugins: {
-//     preflight: true,
-//   },
-//   safelist: [{ pattern: /.*/ }],
-// };
-
-// export default tailwindConfig;
+export default tailwindConfig;
