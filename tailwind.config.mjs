50adc9be93ed07187c281c25dba00b202ac39fc2
Aman kapil <amankapil60@gmail.com>
2025-02-25 11:12:54 +0530

    css change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,26 +18,49 @@
 
 /** @type {import('tailwindcss').Config} */
 const tailwindConfig = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/components/**/*.{js,ts,jsx}", "./src/app/**/*.{js,ts,jsx}"],
   theme: {
     extend: {
       fontFamily: {
         salernomi: ["Salernomi J"],
         inter: ["Inter Display", "sans-serif"],
       },
+      colors: {
+        customBg: "var(--bg-custom)",
+        customText: "var(--text-custom)",
+        darkHover: "var(--text-dark-hover)",
+      },
+      fontSize: {
+        custom64: "var(--text-64)",
+        custom56: "var(--text-56)",
+        custom20: "var(--text-20)",
+      },
+      width: {
+        custom50: "var(--w-60)",
+        custom75: "var(--w-200)",
+        custom90: "var(--w-40)",
+      },
+      height: {
+        custom400: "var(--h-400)",
+        custom600: "var(--h-600)",
+      },
+      padding: {
+        custom10: "var(--p-10)",
+        custom20: "var(--p-20)",
+      },
+      margin: {
+        custom15: "var(--m-15)",
+        custom30: "var(--m-30)",
+      },
+      borderRadius: {
+        customLg: "var(--rounded-lg)",
+      },
     },
   },
   corePlugins: {
     preflight: true,
   },
-  safelist: [
-    { pattern: /.*/ }, // ✅ Allow ALL Tailwind classes, including arbitrary values
-  ],
-  plugins: [require("@tailwindcss/forms")],
+  safelist: [{ pattern: /.*/ }],
 };
 
 export default tailwindConfig;
